edd1db354d2bfe791a66ae969500eb95009e6861
Radek Podrázký <radek.podrazky@green-light.com>
2018-01-25 15:29:57 +0100

    Add - Dashboard component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailsComponent } from './hero-details/hero-details.component';
 import { HeroService } from './hero.service';
 import { AppRoutingModule } from './/app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailsComponent
+    HeroDetailsComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
